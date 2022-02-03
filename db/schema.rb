8d59bc510cc7133b790ae7d74a4dae3531a00f0e
clairedotcom <claireacarson@gmail.com>
2022-02-03 18:21:18 -0500

    Add assocations to User and Event models and generate Devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_171109) do
+ActiveRecord::Schema.define(version: 2022_02_03_175226) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_02_02_171109) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
