11d47d682719478644122fb369478a13a47f48a2
clairedotcom <claireacarson@gmail.com>
2022-02-09 12:29:58 -0500

    Add RegistrationsController and update signup view to include user's name

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+  
   root 'events#index'
+  
   resources :events do
     member do
       get 'attend'
     end
   end
-  
   
   resources :users, only: [:show]
   resources :signups
